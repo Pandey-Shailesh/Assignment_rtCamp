f0388e920e9b7d19833a811abb966ea77e6035b6
Shailesh pandey <pandeyshailesh2081998@gmail.com>
2024-02-12 17:37:33 +0530

    all add to card and other validation are done

--- script.js
+++ script.js
@@ -15,20 +15,61 @@ const { chromium } = require('playwright');
     // Fill login form
     await page.waitForSelector('//input[@id="ap_email"]');
     await page.fill('//input[@id="ap_email"]', 'shaileshpandey2081998@gmail.com');
-    await page.click('//input[@id="continue"]')
+    await page.click('//input[@id="continue"]');
 
     await page.waitForSelector('//input[@id="ap_password"]');
-    await page.fill('//input[@id="ap_password"]','Shailesh@123')
-    await page.click('//input[@id="signInSubmit"]')
+    await page.fill('//input[@id="ap_password"]', 'Shailesh@123');
+    await page.click('//input[@id="signInSubmit"]');
 
 
     // Validation Search Result
     await page.waitForSelector('//input[@id="twotabsearchtextbox"]');
-    await page.fill('//input[@id="twotabsearchtextbox"]','adidas shoes for men')
-    await page.click('//input[@id="nav-search-submit-button"]')
+    await page.fill('//input[@id="twotabsearchtextbox"]', 'men shoes');
+    await page.click('//input[@id="nav-search-submit-button"]');
+
+
+
+
+// Validation Buy Now Result
+await page.waitForSelector('//input[@id="buy-now-button"]');
+await page.click('//input[@id="buy-now-button"]');
+
+await page.waitForSelector('//span[@class="nav-cart-icon"]');
+await page.click('//span[@class="nav-cart-icon"]');
+
+
+await page.waitForSelector('//span[@text=" Go to Cart "]');
+await page.click('//span[@text="Go to Card"]');
+
+await page.waitForSelector('//input[@class="a-button-input "]');
+await page.click('//input[@class="a-button-input "]');
+
+await page.waitForSelector('//input[@name="placeYourOrder1"]');
+await page.click('//input[@name="placeYourOrder1"]');
+
+setTimeout(50000);
+
+     Validate Product Wishlist functionality
+    await page.waitForSelector('//div[@class="_bGlmZ_tint_M0L-m"]');
+    await page.click('//div[@class="_bGlmZ_tint_M0L-m"]');
+
+    await page.waitForSelector('//input[@id="add-to-wishlist-button"]');
+    await page.click('//input[@id="add-to-wishlist-button"]');
+
+    // Scroll down the page
+    await page.evaluate(() => {
+        window.scrollBy(0, 150);
+    });
+
+    await page.waitForSelector('//span[@class=" a-fixed-left-grid-inner"]');
+    await page.click('//span[@class=" a-fixed-left-grid-inner"]');
+
+    await page.waitForSelector('//a[@id="atwl-link-to-list-K3RM5WUP1VOK"]',{timeout:2000});
+    await page.click('//a[@id="atwl-link-to-list-K3RM5WUP1VOK"]');
+
 
     // Close the browser
-    //   await browser.close();
+      await browser.close();
 })();
 
 
